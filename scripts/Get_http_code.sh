2b727e6d1bd3cc9872bec4cea77cb2aa0861b45f
ALJI Mohamed <sim4n6@gmail.com>
2021-03-09 20:36:39 +0100

    update Get_http_code script

--- scripts/Get_http_code.sh
+++ scripts/Get_http_code.sh
@@ -3,4 +3,8 @@
 # A Bash script to get http status of a http link.
 # Author: ALJI Mohamed (aka Sim4n6)
 
-curl -s -o /dev/null -I -w "%{http_code}\n" -- "$1"
+if [[ $1 ]]; then 
+    curl -s -o /dev/null -I -w "%{http_code}\n" -- "$1"
+else
+    printf "Usage:\tGet_http_code.sh www.example.com\n"
+fi
