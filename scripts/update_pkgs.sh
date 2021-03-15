028f7658da5cb179567ad0c18ec4bd9eca83e9c7
ALJI Mohamed <sim4n6@gmail.com>
2021-03-15 21:47:32 +0100

    colors support added

--- scripts/update_pkgs.sh
+++ scripts/update_pkgs.sh
@@ -9,28 +9,32 @@ figlet "A Bash script to update all packages."
 # - place my scripts in /usr/local/bin
 # - update 'secure_path' in /etc/sudoers
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
 if [[ $(id -u) != 0 ]]; then 
-    echo "[-] Please, run me with sudo powers !"
+    echo -e "${RED}[-]${NC} Please, run me with sudo powers !"
     exit 1
 fi
 
-echo -n "[+] Checking connectivity to internet... "
+echo -e -n "${GREEN}[+]${NC} Checking connectivity to internet... "
 if ! ping -c 3 8.8.8.8 &> /dev/null; then 
     echo ""
-    echo "[-] Connectivity problem, please, check your connection."
+    echo -e "${RED}[-]${NC} Connectivity problem, please, check your connection."
     exit 2
 else
     echo "OK"
 fi
 
-echo "[+] Update all packages v0.1"
+echo -e "${GREEN}[+]${NC} Update all packages v0.1"
 apt-get update
 
-echo "[+] List of upgradable packages."
+echo -e "${GREEN}[+]${NC} List of upgradable packages."
 apt list --upgradable
 apt-get -y upgrade
 
-echo "[+] Delete unnecessary packages."
+echo -e "${GREEN}[+]${NC} Delete unnecessary packages."
 apt autoremove
 
 exit 0
