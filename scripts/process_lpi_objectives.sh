0c810a599a5c8fbc313c92a0ff172b82954a5d3a
ALJI Mohamed <sim4n6@gmail.com>
2021-03-19 20:14:28 +0100

    use of dbname and display results.

--- scripts/process_lpi_objectives.sh
+++ scripts/process_lpi_objectives.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sqlite3 topics.db "DROP TABLE IF EXISTS topics;"
-sqlite3 topics.db "CREATE TABLE IF NOT EXISTS topics (topicID INTEGER PRIMARY KEY AUTOINCREMENT, topic VARCHAR(255), weight INTEGER, desc VARCHAR(255) );"
+dbname="objectives.db"
+sqlite3 $dbname "DROP TABLE IF EXISTS topics;"
+sqlite3 $dbname "CREATE TABLE IF NOT EXISTS topics (topicID INTEGER PRIMARY KEY AUTOINCREMENT, topic VARCHAR(255), weight INTEGER, desc VARCHAR(255) );"
 
 declare -a topics;
 declare -a weights;
@@ -38,8 +39,12 @@ i=0
 for e in "${topics[@]}";do 
     echo $e;
     sql="INSERT INTO topics (topic, weight, desc) VALUES (\"${topics[$i]}\",\"${weights[$i]}\", \"${descr[$i]}\" );"
-    sqlite3 topics.db "$sql"
+    sqlite3 $dbname "$sql"
     i=$(( $i+1 ))
 done;
+
+echo "Results :"
+sql="SELECT * FROM topics ORDER BY topics.weight DESC;"
+sqlite3 $dbname "$sql"
 
 rm "$tmpdir"/topics_title.txt "$tmpdir"/weights.txt "$tmpdir"/descriptions.txt
