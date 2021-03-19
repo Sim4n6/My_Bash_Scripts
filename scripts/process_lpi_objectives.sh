e46a926ce6af6db29197016f1f2c3474d6dd1a3d
ALJI Mohamed <sim4n6@gmail.com>
2021-03-19 19:52:41 +0100

    use of mktemp of a directory

--- scripts/process_lpi_objectives.sh
+++ scripts/process_lpi_objectives.sh
@@ -7,29 +7,31 @@ declare -a topics;
 declare -a weights;
 declare -a descr;
 
-cat weight_topics.txt | grep -E "^[0-9].[0-9]" > topics_title.txt
-cat weight_topics.txt | grep "Weight:" | tr -d " " | cut -d ":" -f2  > weights.txt
-cat weight_topics.txt | grep "Description:" | cut -d ":" -f2 > descriptions.txt
+tmpdir=$(mktemp -d)
+
+cat weight_topics.txt | grep -E "^[0-9].[0-9]" > "$tmpdir"/topics_title.txt
+cat weight_topics.txt | grep "Weight:" | tr -d " " | cut -d ":" -f2  > "$tmpdir"/weights.txt
+cat weight_topics.txt | grep "Description:" | cut -d ":" -f2 > "$tmpdir"/descriptions.txt
 
 i=0
 while read -r line; do 
 topics[$i]=$line;
 i=$(( $i+1 ))
-done < topics_title.txt
+done < "$tmpdir"/topics_title.txt
 
 i=0
 while read -r line;
 do 
     weights[$i]=$line
     i=$(( $i+1 ))
-done < weights.txt;
+done < "$tmpdir"/weights.txt;
 
 i=0
 while read -r line;
 do
 descr[$i]=$line
 i=$(( $i+1 ))
-done < descriptions.txt;
+done < "$tmpdir"/descriptions.txt;
 
 
 i=0
@@ -40,4 +42,4 @@ for e in "${topics[@]}";do
     i=$(( $i+1 ))
 done;
 
-rm topics_title.txt weights.txt descriptions.txt
+rm "$tmpdir"/topics_title.txt "$tmpdir"/weights.txt "$tmpdir"/descriptions.txt
